07c0a326ed0a7585463ce593e48e7d41eff6128a
dodiyah330 <dodiyah330@gmail.com>
2024-06-13 23:53:14 +0530

    test: add test for a single number

--- src/app/calculator.test.ts
+++ src/app/calculator.test.ts
@@ -1,7 +1,13 @@
 import { add } from "./calculator";
 
 describe("String Calculator", () => {
+  //test for empty string
   test("should return 0 for an empty string", () => {
     expect(add("")).toBe(0);
+  });
+
+  //test for a single number
+  test("should return the number itself if only one number is provided", () => {
+    expect(add("1")).toBe(1);
   });
 });
--- src/app/calculator.ts
+++ src/app/calculator.ts
@@ -1,7 +1,7 @@
 export function add(numbers: string): number {
-    if (numbers === '') return 0;
-  
-    const numberArray = numbers.split(',').map(Number);
-    return numberArray.reduce((sum, num) => sum + num, 0);
-  }
-  
+  if (numbers === "") return 0;
+  if (!numbers.includes(",")) return parseInt(numbers);
+
+  const numberArray = numbers.split(",").map(Number);
+  return numberArray.reduce((sum, num) => sum + num, 0);
+}
