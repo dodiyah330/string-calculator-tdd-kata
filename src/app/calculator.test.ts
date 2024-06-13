57f572f86668dbf455f4b39c70081e18c10496df
dodiyah330 <dodiyah330@gmail.com>
2024-06-13 23:59:06 +0530

    test : add test for two numbers and multiple numbers

--- src/app/calculator.test.ts
+++ src/app/calculator.test.ts
@@ -10,4 +10,14 @@ describe("String Calculator", () => {
   test("should return the number itself if only one number is provided", () => {
     expect(add("1")).toBe(1);
   });
+
+  //test for two numbers
+  test("should return the sum of two numbers", () => {
+    expect(add("1,5")).toBe(6);
+  });
+
+  //tests for multiple numbers
+  test('should return the sum of multiple numbers', () => {
+    expect(add('1,2,3,4')).toBe(10);
+  });
 });
